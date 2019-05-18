d1d07b877ecba85e310d89267616c37e829202f1
felipeCano <lfcano93@gmail.com>
2019-05-18 18:40:32 -0500

    [navigation] -> addNavigationgrap

---
+++ app/src/main/java/com/comic/universe/ui/ComicFragment.kt
@@ -0,0 +1,12 @@
+package com.comic.universe.ui
+
+import com.comic.universe.R
+import com.comic.universe.center.BaseFragment
+
+class ComicFragment : BaseFragment() {
+
+    override fun onFinishedViewLoad() {
+    }
+
+    override fun fragmentLayout(): Int = R.layout.fragment_comic
+}
