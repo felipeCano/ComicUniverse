90a651917857ffaeba5522a226ddfc3b1b45e18a
felipeCano <lfcano93@gmail.com>
2019-05-28 21:33:53 -0500

    [operations] -> ResolvedOperations

---
+++ app/src/main/java/com/comic/universe/ui/view/operations/OperationsFragment.kt
@@ -0,0 +1,29 @@
+package com.comic.universe.ui.view.operations
+
+import android.os.Bundle
+import com.comic.universe.R
+import com.comic.universe.center.BaseFragment
+import com.comic.universe.util.OperationStrings
+import kotlinx.android.synthetic.main.fragment_operation.*
+
+const val EXPLANATION_USE ="     Explicacion de uso para resolver las operaciones: \n " +
+        "    \"10 + 2 * 6\"            ---> 22 \n" +
+        "     \"100 * 2 + 12\"          ---> 212 \n" +
+        "     \"100 * ( 2 + 12 )\"      ---> 1400 \n" +
+        "     \"100 * ( 2 + 12 ) / 14\" ---> 100     "
+class OperationsFragment : BaseFragment() {
+
+    override fun onFinishedViewLoad() {
+        instructionsForUse.text = EXPLANATION_USE
+
+        btnResolved.setOnClickListener {
+            val responseOperation = OperationStrings.evaluate(evEvaluation.text.toString())
+            val bundle = Bundle()
+            bundle.putInt("responseOperation", responseOperation)
+            navController()!!.navigate(R.id.action_operationsFragment_to_comicFragment)
+            evEvaluation.text.clear()
+        }
+    }
+
+    override fun fragmentLayout(): Int = R.layout.fragment_operation
+}
